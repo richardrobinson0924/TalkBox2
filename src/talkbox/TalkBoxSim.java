ba0f1bba623e587b331780f4fdc2fba5516352ea
Dion <tudion@my.yorku.ca>
2019-02-06 00:55:35 -0500

    Adding the window for the Simulator (exactly like the configurator)

--- src/talkbox/TalkBoxSim.java
+++ src/talkbox/TalkBoxSim.java
@@ -1,6 +1,7 @@
 package talkbox;
 
 import javafx.application.Application;
+import javafx.stage.Stage;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -9,11 +10,19 @@ import java.io.ObjectOutputStream;
 /**
  * @apiNote DO NOT use <code>audioFilenames[i][j]</code> field to retrieve filenames. Instead, use <code>getPath(i, j)</code>. This is because each filename in the matrix is a pseudomap, of form <code>"actualFilename|alias"</code>.
  */
-public class TalkBoxSim {
+public class TalkBoxSim extends Application {
     // instructions:
     // you wanna ask the user if they wanna make a new tbc file or open an existing one. (2 buttons)
     // when the .tbc file is created, it opens (or when the existing file is opened) and a simple interface of the buttons
-    public static void main(String... args) throws IOException {
+    public static void main(String... args) {
+
+    }
+
+    public void start(Stage primaryStage) {
+
+    }
+
+    public void createNewTBC() throws IOException {
         FileOutputStream fos = new FileOutputStream("test.tbc");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         TalkBoxData ts = new TalkBoxData();
